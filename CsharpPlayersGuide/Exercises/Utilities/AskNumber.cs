b0b9e199b11094fc39d0cd9867d7222f632efb0b
ldctaylor <ldctaylor@gmail.com>
2024-04-01 05:28:39 +1000

    Add utilities folder and create helper for asking for number input

---
+++ CsharpPlayersGuide/Exercises/Utilities/AskNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpPlayersGuide.Exercises.Utilities
+{
+    internal class AskNumber
+    {
+        public static int AskForNumber(string message)
+        {
+            int number;
+            bool success;
+
+            Console.WriteLine(message);
+
+            {
+                while (true)
+                {
+                    string? input = Console.ReadLine();
+                    success = int.TryParse(input, out number);
+
+                    if (success)
+                    {
+                        return number;
+                    }
+                    else
+                    {
+                        Console.WriteLine("I told you to use a valid number!!! Try again");
+                    }
+                }
+        }
+    }
+    }
+}
